0676cd14630f975f1f1b9eb23b82aa22e4f8aef9
FeliciaBo <theskylla@gmail.com>
2026-04-15 14:30:50 +0200

    testing stategy

--- wordle-app/tests/game.spec.js
+++ wordle-app/tests/game.spec.js
@@ -1,24 +1,51 @@
 /* Testning:
 
-Mockord: "melon" (ingen duplicate, 5 bokstäver)
-
-- "happy path" med rätt gissning
-   - Alla bokstäver är gröna = spelet är över
-   - möjlighet att starta nytt spel
-   (-ingent skickas till highscore)
-
-
-
-Mockord: "banan" (duplicate tillåten, 5 bokstäver)  
-
-Mockord: "bär" (ingen duplicate, 3 bokstäver)
-
-
 Spel:
 
-- Spel slut:
- - tid
- - antal gissningar
- - Valda filter: antal bokstäver, 
+-Innan spel:
+ - Filtrering: 
+   - Val av antal bokstäver 
+   - Val av duplicate eller inte
+ - Startknapp
 
+ -Under spel: 
+ - Gissning
+ - Feedback på gissning (grön/correct, gul/misplaced, röd/incorrect)
+
+ - Spel slut:
+ - tid (från backend)
+ - antal gissningar
+ - Valda filter: antal bokstäver, unique
+
+ Highscore:
+- Spelresultatet finns i highscore-listan
+
+
+
+Mockord: 
+
+   Lista med mockord: "melon, banan, bär" (en för respektive inställning)
+   - "melon" (default: ingen duplicate, 5 bokstäver)
+   -  "banan" (duplicate tillåten, 5 bokstäver)
+   - "bär" (ingen duplicate, 3 bokstäver)
+
+
+ Tester:
+
+   Gissningar:
+   - Rätt gissning direkt:
+     - Alla bokstäver är gröna = spelet är över
+
+   - Fel gissning: 
+     - Några bokstäver är gröna, några gula, några röda
+     - möjlighet att gissa igen
+   
+   Spel slut:
+   - rätt spelresultat hämtas från backend
+   - möjlighet att starta nytt spel
+   - skicka spelresultat till highscores eller inte
+
+   Highscore:
+   - Om skickat: Resultatet finns i listan
+  
 */
