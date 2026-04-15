8266c421663e6fb1f0bdc1bca7726fb568ff8ea8
FeliciaBo <theskylla@gmail.com>
2026-04-15 14:01:37 +0200

    changed global scss path

---
+++ wordle-app/tests/game.spec.js
@@ -0,0 +1,24 @@
+/* Testning:
+
+Mockord: "melon" (ingen duplicate, 5 bokstäver)
+
+- "happy path" med rätt gissning
+   - Alla bokstäver är gröna = spelet är över
+   - möjlighet att starta nytt spel
+   (-ingent skickas till highscore)
+
+
+
+Mockord: "banan" (duplicate tillåten, 5 bokstäver)  
+
+Mockord: "bär" (ingen duplicate, 3 bokstäver)
+
+
+Spel:
+
+- Spel slut:
+ - tid
+ - antal gissningar
+ - Valda filter: antal bokstäver, 
+
+*/
