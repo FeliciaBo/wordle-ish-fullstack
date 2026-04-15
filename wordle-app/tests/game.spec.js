8d16a7bc8be6033e35d4d334afa83be0ec5c407b
FeliciaBo <theskylla@gmail.com>
2026-04-15 14:50:48 +0200

    Shortened test strategy

--- wordle-app/tests/game.spec.js
+++ wordle-app/tests/game.spec.js
@@ -2,50 +2,42 @@
 
 Spel:
 
--Innan spel:
- - Filtrering: 
+1. Innan spel:
+(- Sidan startas upp utan fel)
+ - Default-filtrering är korrekt, men kan ändras: 
    - Val av antal bokstäver 
    - Val av duplicate eller inte
- - Startknapp
+ - Startknapp startar spelet och hämtar ett ord 
 
- -Under spel: 
- - Gissning
+ 2. Under spel: 
+ - Spelaren gissar genom att skriva i input-rutan och klicka på submit-knappen
  - Feedback på gissning (grön/correct, gul/misplaced, röd/incorrect)
+ - Om fel gissining: möjlighet att gissa igen
+ - Om rätt gissning: spelet är slut (nästa steg)
 
- - Spel slut:
- - tid (från backend)
- - antal gissningar
- - Valda filter: antal bokstäver, unique
+3. Spel slut:
+ - "Du vann"-text
+ - Spelresultat visas:  
+    - tid (från backend)
+    - antal gissningar
+    - rätt ord
+    - Valda filter:
+      - antal bokstäver
+      - unique eller ej
 
- Highscore:
-- Spelresultatet finns i highscore-listan
+ - Knapp och input för att skicka in resultat till highscore
+ - Knapp för att starta nytt spel
+   - Fungerar efter att skicka in till highscore
+   - Fungerat utan att skicka in till hioghscore
+
+4. Highscore:
+ - Om skickat: Spelresultatet finns i highscore-listan
 
 
-
-Mockord: 
-
-   Lista med mockord: "melon, banan, bär" (en för respektive inställning)
+  Mockord:
    - "melon" (default: ingen duplicate, 5 bokstäver)
    -  "banan" (duplicate tillåten, 5 bokstäver)
    - "bär" (ingen duplicate, 3 bokstäver)
 
-
- Tester:
-
-   Gissningar:
-   - Rätt gissning direkt:
-     - Alla bokstäver är gröna = spelet är över
-
-   - Fel gissning: 
-     - Några bokstäver är gröna, några gula, några röda
-     - möjlighet att gissa igen
    
-   Spel slut:
-   - rätt spelresultat hämtas från backend
-   - möjlighet att starta nytt spel
-   - skicka spelresultat till highscores eller inte
-
-   Highscore:
-   - Om skickat: Resultatet finns i listan
-  
 */
